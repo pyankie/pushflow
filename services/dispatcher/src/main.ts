086a51b933c59280ee5ebb594dbe6b081ed1b995
pyankie <zpyankie@gmail.com>
2025-12-21 19:46:29 +0300

    feat(dispatcher): load env vars

--- services/dispatcher/src/main.ts
+++ services/dispatcher/src/main.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await NestFactory.createApplicationContext(AppModule);
 }
 bootstrap();
