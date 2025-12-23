e722f307f025044e8a06650ddb93731233beade8
pyankie <zpyankie@gmail.com>
2025-12-23 13:12:54 +0300

    refactor(delivery): remove unused nestjs bolierplates

--- services/delivery/src/app.module.ts
+++ services/delivery/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConnectionModule } from './modules/connections/connections.module';
-import { RedisModule } from './modules/redis/redis.module';
 import { DeliveryModule } from './modules/delivery/delivery.module';
+import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
   imports: [ConnectionModule, RedisModule, DeliveryModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
