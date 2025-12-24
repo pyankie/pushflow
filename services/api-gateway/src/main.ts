d75076e12f0a7868c203ec597bb13d0795e05929
pyankie <zpyankie@gmail.com>
2025-12-24 11:07:55 +0300

    feat(api-gateway): setup env vars

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.GATEWAY_PORT || 9090;
+  await app.listen(port);
+
+  console.log(`API Gateway is running on port ${port}`);
 }
 bootstrap();
