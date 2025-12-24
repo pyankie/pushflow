ec29d8f942cbb413b28549bc3dab3dc7ec119a74
pyankie <zpyankie@gmail.com>
2025-12-24 14:14:39 +0300

    feat(api-gateway): register notification module

--- services/api-gateway/src/app.module.ts
+++ services/api-gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './modules/redis/redis.module';
+import { NotificationModule } from './modules/notifications/notification.module';
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ services/api-gateway/src/modules/notifications/notification.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RedisModule } from '../redis/redis.module';
+import { NotificationController } from './notification.controller';
+import { NotificationService } from './notification.service';
+
+@Module({
+  imports: [RedisModule],
+  providers: [NotificationService],
+  controllers: [NotificationController],
+})
+export class NotificationModule {}
