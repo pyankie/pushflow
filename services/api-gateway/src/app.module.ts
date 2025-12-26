8f984c65969d41b784c5af095c48de92b55ed8e3
pyankie <zpyankie@gmail.com>
2025-12-26 17:32:35 +0300

    refactor(api-gateway): cleanup unused imports

--- services/api-gateway/src/app.module.ts
+++ services/api-gateway/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RedisModule } from './modules/redis/redis.module';
 import { NotificationModule } from './modules/notifications/notification.module';
 
 @Module({
   imports: [RedisModule, NotificationModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
